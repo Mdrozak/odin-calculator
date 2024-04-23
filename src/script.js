222d6a00edcca9616f347e23a6ac445ab36948da
Michał <108900680+Mdrozak@users.noreply.github.com>
2024-04-23 22:26:26 +0200

    Fix script link, add basic functions of future calculator

---
+++ src/script.js
@@ -0,0 +1,19 @@
+// Create function that adds numbers
+function add (number1, number2) {
+    return number1 + number2
+}
+
+// Create function that subtract numbers
+function subtract (number1, number2) {
+    return number1 - number2
+}
+
+// Create function that multiply numbers
+function multiply (number1, number2) {
+    return number1 * number2
+}
+
+// Create function that divide numbers
+function divide (number1, number2) {
+    return number1 / number2
+}
